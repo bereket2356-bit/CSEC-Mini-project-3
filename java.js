bd660c2ba75a1b0565431d1f390318ce7fc4bc29
Bereket Teshome <bereket2356@gmail.com>
2025-12-19 12:44:20 +0300

    third project

--- java.js
+++ java.js
@@ -1,11 +1,15 @@
 const click=document.getElementsByClassName("toggle");
-const text=document.getElementById("1");
+const text=document.getElementById("text");
 const head=document.getElementById("head");
+const img=document.getElementById("img");
+
 function myfunction(){
   if(head.textContent=="OFF"){
     head.textContent="ON";
+  
   }else if(head.textContent=="ON"){
     head.textContent="OFF";
+ 
   }else{
     head.textContent="OFF";
   }
